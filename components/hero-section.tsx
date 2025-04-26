2100129ddd9b5254913b343c11057d35128ee4ec
Shubhamsharma3092000 <your-email@associated-with-shubhamsharma3092000>
2025-04-26 14:51:18 +0530

    text ani

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import React, { useEffect, useRef } from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
-
+import { motion } from "motion/react";
 export default function HeroSection() {
   const imageRef = useRef<HTMLDivElement | null>(null)
   useEffect(()=>{
@@ -26,7 +26,33 @@ export default function HeroSection() {
     <section className='w-full pt-24 md:pt-28 pb-10 overflow-hidden bg-gradient-to-b'>
       <div className=' space-y-6 text-center'>
       <div className='space-y-6 mx-auto'>
-        <h1 className='gradient-title text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl'>Your Personal AI Assistant for
+        <h1 className='gradient-title text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl'>Your Personal AI <span>
+        <span className=" text-yellow-50 ">
+        <h1 className="max-w-2xl text-center  lg:text-6xl xl:text-8xl leading-snug inline-block">
+        <span className="relative">
+          Assistant
+          <svg
+            viewBox="0 0 286 73"
+            fill="none"
+            className="absolute -left-2 -right-2 -top-2 bottom-0 translate-y-1"
+          >
+            <motion.path
+              initial={{ pathLength: 0 }}
+              whileInView={{ pathLength: 1 }}
+              transition={{
+                duration: 1.25,
+                ease: "easeInOut",
+              }}
+              d="M142.293 1C106.854 16.8908 6.08202 7.17705 1.23654 43.3756C-2.10604 68.3466 29.5633 73.2652 122.688 71.7518C215.814 70.2384 316.298 70.689 275.761 38.0785C230.14 1.37835 97.0503 24.4575 52.9384 1"
+              stroke="#48A6A7"
+              strokeWidth="2"
+            />
+          </svg>
+        </span>{" "}
+ 
+      </h1>
+      </span>
+          </span> for
         <br />
         Professional Success
         </h1>
